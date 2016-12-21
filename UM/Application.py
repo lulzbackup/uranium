f90c65ffe1b80de82efe6df1395c8a5f02b41939
Victor Larchenko <victor99992@gmail.com>
2016-12-21 13:08:44 +0600

    T485: Added closing confirmation dialog

--- UM/Application.py
+++ UM/Application.py
@@ -138,6 +138,9 @@ class Application():
     def showMessage(self, message):
         raise NotImplementedError
 
+    def isExitAllowed(self):
+        return True
+
     ##  Get the version of the application
     #   \returns version \type{string}
     def getVersion(self):
